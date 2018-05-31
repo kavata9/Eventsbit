8bd05eba5abb7122adfbd8e9d15d609d82cb8a6a
agnes musyoka <kavatamusyoka24@gmail.com>
2018-05-31 09:40:17 +0300

    add templates folder

---
+++ src/main/java/App.java
@@ -0,0 +1,27 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App{
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("sign", request.session().attribute("sign"));
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
+   get("/eventbrowse", (request, response) -> {
+     Map<String, Object> model = new HashMap<String, Object>();
+     model.put("sign", request.session().attribute("sign"));
+     model.put("template", "templates/eventbrowse.vtl");
+     return new ModelAndView(model, layout);
+   }, new VelocityTemplateEngine());
+ }
+}
